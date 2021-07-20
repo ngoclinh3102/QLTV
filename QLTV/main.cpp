888bf33a8c6e47edaeac44ee6166f470d7b8362c
ngoclinh3102 <83945131+ngoclinh3102@users.noreply.github.com>
2021-07-20 23:04:45 +0700

    by Linh
    update doc ghi file

---
+++ QLTV/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "GiaoDien.h"
+#include "QLDG.h"
+using namespace std;
+
+int main() {
+	//===DOC FILE
+	DocDSMaThe();
+	DocDSDocGia();
+	//===XU LI
+	bool flag= true;
+	while(flag)
+	{
+		SetWindow();
+		int chon;
+		chon = MenuDong(Menu);
+		switch(chon)
+		{
+			case 1: {
+				//cout<<"quan li doc gia";
+				runQLDG();
+				break;
+			}
+			case 2: {
+				cout<<"quan li dau sach";
+				break;
+			}
+			case 3: {
+				cout<<"muon/tra sach";
+				break;
+			}
+			case 4: {
+				cout<<"good bye\n\n";
+				flag= false;
+				break;
+			}
+		}
+//	system("pause");
+	}
+	system("cls");
+	//ghi file
+	GhiDSDocGia();
+	GhiDSMaThe();
+	Sleep(500);
+	
+//	system("pause");
+	return 0;
+}
