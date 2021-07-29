2b820d5049394f0ab90245a21614bf4b44e17ce8
ngoclinh3102 <83945131+ngoclinh3102@users.noreply.github.com>
2021-07-29 07:45:24 +0700

    new update
    bắt điều kiện nhập đầu sách, tìm kiếm theo tên, đọc ghi file đầu sách
    missing: sửa + xóa đầu sách, nhập + sửa + xóa dms

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "QLDG.h"
+#include "QLDauSach.h"
+
+using namespace std;
+
+int main() {
+	//===DOC FILE
+	DocDSMaThe();
+	DocDSDocGia();
+	DocDSDauSach();
+	
+	//===XU LI
+	bool flag= true;
+	while(flag)
+	{
+		SetWindow();
+		int chon;
+		chon = MenuDong(Menu);
+		switch(chon)
+		{
+			case 1: {
+				//cout<<"quan li doc gia";
+				runQLDG();
+				break;
+			}
+			case 2: {
+//				cout<<"quan li dau sach";
+				runQLDS();
+				break;
+			}
+			case 3: {
+				cout<<"muon/tra sach";
+				break;
+			}
+			case 4: {
+				cout<<"good bye\n\n";
+				flag= false;
+				break;
+			}
+		}
+//	system("pause");
+	}
+	system("cls");
+	//ghi file
+	GhiDSDocGia();
+	GhiDSMaThe();
+	GhiDSDauSach();
+	
+//	system("pause");
+	return 0;
+}
